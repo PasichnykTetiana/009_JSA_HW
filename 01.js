043f0849233c93efb8c64f2967cbf461982d08b1
Tetiana <t.pasichnyk@ukr.net>
2020-07-14 21:19:49 +0300

    1/3

---
+++ 01.js
@@ -0,0 +1,10 @@
+let img = document.getElementById("pic");
+document.getElementById("button1").addEventListener("click", function () {
+img.src = "1.jpg"
+})
+document.getElementById("button2").addEventListener("click", function () {
+    img.src = "2.jpg"
+})
+document.getElementById("button3").addEventListener("click", function () {
+    img.src = "3.jpg"
+})
